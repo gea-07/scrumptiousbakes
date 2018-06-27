c4e7488a8ff6777d2cb14e82ea1a638de2a8eff9
Arlene Gustafson <arlene@gustafsonfamily.org>
2018-06-26 18:40:26 -0600

    Initial project commit

---
+++ src/main/java/com/arleneg/android/scrumptiousbakes/MainActivity.java
@@ -0,0 +1,13 @@
+package com.arleneg.android.scrumptiousbakes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
