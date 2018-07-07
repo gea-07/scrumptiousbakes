c5504a2165366fe0179566a02ea57e69b2bbb4b4
Arlene Gustafson <arlene@gustafsonfamily.org>
2018-07-07 13:30:59 -0600

    add_mainactivity_error_handling: Added error handling in main activity and calculated span for grid layout manager.

--- src/main/java/com/arleneg/android/scrumptiousbakes/ui/MainActivity.java
+++ src/main/java/com/arleneg/android/scrumptiousbakes/ui/MainActivity.java
@@ -2,10 +2,13 @@ package com.arleneg.android.scrumptiousbakes.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,10 +16,9 @@ import com.arleneg.android.scrumptiousbakes.R;
 import com.arleneg.android.scrumptiousbakes.data.Recipe;
 import com.arleneg.android.scrumptiousbakes.network.RecipeService;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,6 +33,12 @@ public class MainActivity extends AppCompatActivity implements RecipeListAdapter
     @BindView(R.id.main_activity_recycler_view)
     RecyclerView mRecipeRecyclerView;
 
+    @BindView(R.id.error_results_not_found_text_view)
+    TextView mErrorTextView;
+
+    @BindView(R.id.data_loading_progress_bar)
+    ProgressBar mLoadDataProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +47,8 @@ public class MainActivity extends AppCompatActivity implements RecipeListAdapter
         ButterKnife.bind(this);
 
         // setup the recycler view
-        mRecipeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        int span = calculateBestSpanCount(1000);
+        mRecipeRecyclerView.setLayoutManager(new GridLayoutManager(this, (span < 1) ? 1 : span));
         mAdapter = new RecipeListAdapter(this);
         mRecipeRecyclerView.setAdapter(mAdapter);
 
@@ -47,8 +56,23 @@ public class MainActivity extends AppCompatActivity implements RecipeListAdapter
         getRecipeData();
     }
 
+    // calculateBestSpanCount was copied from fellow classmate's blog:
+    // http://programmingbackwards.blogspot.com/2018/06/when-popularmoviesapp-has-worn-you-down.html
+    private int calculateBestSpanCount(int posterWidth) {
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
+        float screenWidth = outMetrics.widthPixels;
+        return Math.round(screenWidth / posterWidth);
+    }
+
+    private void showErrorMessage (Boolean showError) {
+        mRecipeRecyclerView.setVisibility(showError ? View.INVISIBLE : View.VISIBLE);
+        mErrorTextView.setVisibility(showError ? View.VISIBLE : View.INVISIBLE);
+    }
+
     private void getRecipeData() {
-//        mLoadDataProgressBar.setVisibility(View.VISIBLE);
+        mLoadDataProgressBar.setVisibility(View.VISIBLE);
         RecipeService recipeService = RecipeService.retrofit.create(RecipeService.class);
         Log.d(TAG, "Loading recipe data from the network.");
         Call<List<Recipe>> call = recipeService.getRecipes();
@@ -56,27 +80,26 @@ public class MainActivity extends AppCompatActivity implements RecipeListAdapter
         call.enqueue(new Callback<List<Recipe>>() {
             @Override
             public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
-//                mLoadDataProgressBar.setVisibility(View.INVISIBLE);
+                mLoadDataProgressBar.setVisibility(View.INVISIBLE);
+                showErrorMessage(false);
                 Log.d(TAG, "Inside onResponse for Retrofit call.");
                 if (response.isSuccessful()) {
                     if (response.body() != null) {
                         ArrayList<Recipe> recipes = (ArrayList<Recipe>) response.body();
                         mAdapter.setRecipeList(recipes);
-//                        mMovieImagesAdapter.setMovieData(mMovieData);
-//                        showMovies();
                     }
                 } else {
-//                    mErrorTextView.setText("Network error loading movie database API: " + response.code());
-//                    showErrorMessage();
+                    mErrorTextView.setText(String.format(Locale.getDefault(),"%s %d", getString(R.string.network_error_msg), response.code()));
+                    showErrorMessage(true);
                     Log.d(TAG, "Inside onResponse...response not successful.");
                 }
             }
 
             @Override
             public void onFailure(Call<List<Recipe>> call, Throwable t) {
-//                mLoadDataProgressBar.setVisibility(View.INVISIBLE);
-//                mErrorTextView.setText("Error loading from the Movie DB Api. Your internet connection may be down. Please try again later.");
-//                showErrorMessage();
+                mLoadDataProgressBar.setVisibility(View.INVISIBLE);
+                mErrorTextView.setText(getString(R.string.network_not_available));
+                showErrorMessage(true);
                 Log.d(TAG, "Inside onFailure.");
             }
         });
@@ -84,6 +107,7 @@ public class MainActivity extends AppCompatActivity implements RecipeListAdapter
 
     @Override
     public void onItemClick(int item) {
+        Toast.makeText(this, mAdapter.getRecipeList().get(item).getName(), Toast.LENGTH_SHORT).show();
         Log.d(TAG, "Clicked row " + item);
     }
 }
