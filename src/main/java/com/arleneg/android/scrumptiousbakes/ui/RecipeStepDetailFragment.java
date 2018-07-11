f1270ab283dcb0c9c1e0dc6de428559e82c5e6f2
Arlene Gustafson <arlene@gustafsonfamily.org>
2018-07-10 22:23:08 -0600

    add_exoplayer: Added ExoPlayer to play recipe video

--- src/main/java/com/arleneg/android/scrumptiousbakes/ui/RecipeStepDetailFragment.java
+++ src/main/java/com/arleneg/android/scrumptiousbakes/ui/RecipeStepDetailFragment.java
@@ -1,6 +1,7 @@
 package com.arleneg.android.scrumptiousbakes.ui;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +11,13 @@ import android.widget.TextView;
 
 import com.arleneg.android.scrumptiousbakes.R;
 import com.arleneg.android.scrumptiousbakes.data.Step;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,7 +30,11 @@ public class RecipeStepDetailFragment extends Fragment {
     @BindView(R.id.step_description_tv)
     TextView mStepDescription;
 
+    @BindView(R.id.player_view)
+    PlayerView mPlayerView;
+
     private Step mStep;
+    private SimpleExoPlayer mPlayer;
 
     public RecipeStepDetailFragment() {
         // Required empty public constructor
@@ -37,7 +49,36 @@ public class RecipeStepDetailFragment extends Fragment {
         ButterKnife.bind(this, view);
 
         mStepDescription.setText(mStep.getDescription());
+
+        // section on ExoPlayer is inspired and copied from https://www.youtube.com/watch?v=svdq1BWl4r8
+        // Building feature-rich media apps with ExoPlayer (Google I/O '18)
+        if (mStep.getVideoURL().isEmpty()) {
+            mPlayerView.setVisibility(View.INVISIBLE);
+        }
+        else {
+            mPlayerView.setVisibility(View.VISIBLE);
+            mPlayer = ExoPlayerFactory.newSimpleInstance(view.getContext(), new DefaultTrackSelector());
+            mPlayerView.setPlayer(mPlayer);
+            DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(view.getContext(),
+                    Util.getUserAgent(view.getContext(), "scrumptiousbakes"));
+            Uri videoUri = Uri.parse(mStep.getVideoURL());
+            ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(videoUri);
+            mPlayer.prepare(mediaSource);
+            mPlayer.setPlayWhenReady(true);
+        }
+
         return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (!mStep.getVideoURL().isEmpty()) {
+            mPlayerView.setPlayer(null);
+            mPlayer.release();
+            mPlayer = null;
+        }
     }
 
     public void setStep(Step step) {
